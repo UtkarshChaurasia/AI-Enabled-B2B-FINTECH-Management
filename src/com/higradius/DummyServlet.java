0979b150697586f226576cc54b3abbeba3d8af95
Utkarsh Chaurasia <utkarshchaurasia@pop-os.localdomain>
2021-07-09 21:30:37 +0530

    initial setup

---
+++ src/com/higradius/DummyServlet.java
@@ -0,0 +1,56 @@
+package com.higradius;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+/**
+ * Servlet implementation class dummyServlet
+ */
+@WebServlet("/dummyServlet")
+public class DummyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DummyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String name = request.getParameter("name");
+		Response resp = new Response();
+		resp.setName(name);
+		Gson gson = new Gson();
+		 String data = gson.toJson(resp);
+		 PrintWriter out = response.getWriter();
+		 response.setContentType("application/json");
+		 response.setCharacterEncoding("UTF-8");
+		 out.print(data);
+		 out.flush();
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/com/higradius/Response.java
@@ -0,0 +1,13 @@
+package com.higradius;
+
+public class Response {
+String name;
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+}
