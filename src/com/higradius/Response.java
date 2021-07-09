aa8c894def246bb78fc166f8231fef1c9ea75a4e
Utkarsh Chaurasia <utkarshchaurasia@pop-os.localdomain>
2021-07-09 21:47:35 +0530

    pojo class

--- src/com/higradius/Response.java
+++ src/com/higradius/Response.java
@@ -1,13 +1,81 @@
 package com.higradius;
 
 public class Response {
-String name;
+private int field;
+private String customerName;
+private  String customerId;
+private String invoiceId;
+private float invoiceAmt;
+private String dueDate;
+private String predictedDate;
+private String notes;
 
-public String getName() {
-	return name;
+public int getField() {
+	return field;
 }
 
-public void setName(String name) {
-	this.name = name;
+public void setField(int field) {
+	this.field = field;
 }
+
+public String getCustomerName() {
+	return customerName;
+}
+
+public void setCustomerName(String customerName) {
+	this.customerName = customerName;
+}
+
+
+public String getCustomerId() {
+	return customerId;
+}
+
+public void setCustomerId(String customerId) {
+	this.customerId = customerId;
+}
+
+public String getInvoiceId() {
+	return invoiceId;
+}
+
+public void setInvoiceId(String invoiceId) {
+	this.invoiceId = invoiceId;
+}
+
+public float getInvoiceAmt() {
+	return invoiceAmt;
+}
+
+public void setInvoiceAmt(float invoiceAmt) {
+	this.invoiceAmt = invoiceAmt;
+}
+
+public String getDueDate() {
+	return dueDate;
+}
+
+public void setDueDate(String dueDate) {
+	this.dueDate = dueDate;
+}
+
+public String getPredictedDate() {
+	return predictedDate;
+}
+
+public void setPredictedDate(String predictedDate) {
+	this.predictedDate = predictedDate;
+}
+
+public String getNotes() {
+	return notes;
+}
+
+public void setNotes(String notes) {
+	this.notes = notes;
+}
+
+
+
+
 }
